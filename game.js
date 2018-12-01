b21481e737f822b9ad570649366648ff619b0cef
yennanliue <f339339@gmail.com>
2018-12-01 14:33:20 +0000

    modify canvas width

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width =  700;
+        this.canvas.width =  500;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);

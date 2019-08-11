af1ac75ea1744890090100a819aec3e657afd615
Travis CI <f339339@gmail.com>
2019-08-11 11:22:46 +0800

    add createModel func and imort model to main run func

--- workspace/script.js
+++ workspace/script.js
@@ -31,8 +31,24 @@ async function run() {
       height: 300
     }
   );
-
-  // More code will be added below
+  
+  // Create the model
+  const model = createModel();  
+  tfvis.show.modelSummary({name: 'Model Summary'}, model);
 }
 
+function createModel() {
+  // Create a sequential model
+  const model = tf.sequential(); 
+  
+  // Add a single hidden layer
+  model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+  
+  // Add an output layer
+  model.add(tf.layers.dense({units: 1, useBias: true}));
+
+  return model;
+}
+
+// run the func 
 document.addEventListener('DOMContentLoaded', run);

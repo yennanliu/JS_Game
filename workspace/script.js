e50679c176cde16dcb0fb8c14773a45ff6d7d196
Travis CI <f339339@gmail.com>
2019-08-11 11:54:15 +0800

    add testModel func

--- workspace/script.js
+++ workspace/script.js
@@ -54,6 +54,50 @@ function convertToTensor(data) {
   });  
 }
 
+function testModel(model, inputData, normalizationData) {
+  const {inputMax, inputMin, labelMin, labelMax} = normalizationData;  
+  
+  // Generate predictions for a uniform range of numbers between 0 and 1;
+  // We un-normalize the data by doing the inverse of the min-max scaling 
+  // that we did earlier.
+  const [xs, preds] = tf.tidy(() => {
+    
+    const xs = tf.linspace(0, 1, 100);      
+    const preds = model.predict(xs.reshape([100, 1]));      
+    
+    const unNormXs = xs
+      .mul(inputMax.sub(inputMin))
+      .add(inputMin);
+    
+    const unNormPreds = preds
+      .mul(labelMax.sub(labelMin))
+      .add(labelMin);
+    
+    // Un-normalize the data
+    return [unNormXs.dataSync(), unNormPreds.dataSync()];
+  });
+  
+ 
+  const predictedPoints = Array.from(xs).map((val, i) => {
+    return {x: val, y: preds[i]}
+  });
+  
+  const originalPoints = inputData.map(d => ({
+    x: d.horsepower, y: d.mpg,
+  }));
+  
+  
+  tfvis.render.scatterplot(
+    {name: 'Model Predictions vs Original Data'}, 
+    {values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
+    {
+      xLabel: 'Horsepower',
+      yLabel: 'MPG',
+      height: 300
+    }
+  );
+}
+
 /**
  * Get the car data reduced to just the variables we are interested
  * and cleaned of missing data.
@@ -123,6 +167,8 @@ async function run() {
   // Train the model  
   await trainModel(model, inputs, labels);
   console.log('Done Training');
+  // Test the model 
+  testModel(model, data, tensorData);
 }
 
 // run the func 
